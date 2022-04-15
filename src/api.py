cd42dc74c338354e8efdbdfb0fda2ea0a3128979
Mark Breen <markvader@users.noreply.github.com>
2022-04-15 01:51:34 +0100

    additions to signals and sonics api calls
    store response to variables

--- src/api.py
+++ src/api.py
@@ -23,13 +23,13 @@ from const import (
 
 
 class Api:
-
+        
         # Acquiring an access token is a one-step process.
         # Send an authorizing request with your credentials (email & password).
         # All other API endpoint requests must be authenticated with an access token 
         # that is put in the authorization header using the Bearer scheme.
         # A client may have up to 10 active tokens at a time.
-        
+
     def __init__(self, herolabs_email: str, herolabs_password: str):
         """
         param (str) herolabs_email = the herolabs user email
@@ -40,6 +40,32 @@ class Api:
         self._user_id: Optional[str] = None
         self._auth_token: Optional[str] = None
         self._auth_token_expiration: Optional[datetime] = None
+        self._property_id: Optional[str] = None
+        self._incident_id: Optional[str] = None
+
+        self._sonic_total_sonics: Optional[int] = None
+        self._sonic_id: Optional[str] = None
+        self._sonic_auto_shutoff_enabled: Optional[bool] = None
+        self._sonic_auto_shutoff_time_limit: Optional[int] = None
+        self._sonic_auto_shutoff_volume_limit: Optional[int] = None
+        self._sonic_battery_level: Optional[str] = None
+        self._sonic_name: Optional[str] = None
+        self._sonic_radio_connection: Optional[str] = None
+        self._sonic_radio_rssi: Optional[int] = None
+        self._sonic_serial_no: Optional[str] = None
+        self._sonic_status: Optional[str] = None
+        self._sonic_valve_state: Optional[str] = None
+
+        self._signal_total_signals: Optional[int] = None
+        self._signal_id: Optional[str] = None
+        self._signal_boot_time: Optional[int] = None
+        self._signal_cloud_connection: Optional[str] = None
+        self._signal_modem_boot_time: Optional[int] = None
+        self._signal_modem_version: Optional[str] = None
+        self._signal_name: Optional[str] = None
+        self._signal_serial_no: Optional[str] = None
+        self._signal_version: Optional[str] = None
+        self._signal_wifi_rssi: Optional[int] = None
 
     def _retrieve_token(self) -> None:
         """this sends a request to get an auth token"""
@@ -54,10 +80,9 @@ class Api:
         response_data = response.json()
         self._auth_token = response_data["token_details"]
         self._user_id = response_data["user_details"]["id"]
-        
-        # print(response.text)
+        print(response.text)
 
-    def _user_details(self) -> None:
+    def _user_details(self):
         user_details_url = USER_RESOURCE + self._user_id
         response = requests.get(
             user_details_url,
@@ -67,4 +92,71 @@ class Api:
             }
         )
         data = response.json()
-        print("user_id json response: "+response.text)
+
+        print(response.text)
+        return data
+
+    def _retrieve_sonics(self) -> None:
+        """this sends a request to get a list of sonic devices token"""
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self._auth_token}"
+        }
+        response = requests.get(LIST_SONICS_RESOURCE, headers=headers)
+        response_data = response.json()
+        print(response_data)
+        self._sonic_total_sonics = response_data["total_entries"] #need to do something if total sonics > 1
+        self._sonic_id = response_data["data"][0]["id"]
+        self._sonic_auto_shutoff_enabled = response_data["data"][0]["auto_shut_off_enabled"]
+        self._sonic_auto_shutoff_time_limit = response_data["data"][0]["auto_shut_off_time_limit"]
+        self._sonic_auto_shutoff_volume_limit = response_data["data"][0]["auto_shut_off_volume_limit"]
+        self._sonic_battery_level = response_data["data"][0]["battery"]
+        self._sonic_name = response_data["data"][0]["name"]
+        self._sonic_radio_connection = response_data["data"][0]["radio_connection"]
+        self._sonic_radio_rssi = response_data["data"][0]["radio_rssi"]
+        self._sonic_serial_no = response_data["data"][0]["serial_no"]
+        self._sonic_status = response_data["data"][0]["status"]
+        self._sonic_valve_state = response_data["data"][0]["valve_state"]
+        # print(self._sonic_total_sonics)
+        # print(self._sonic_id)
+        # print(self._sonic_auto_shutoff_enabled)
+        # print(self._sonic_auto_shutoff_time_limit)
+        # print(self._sonic_auto_shutoff_volume_limit)
+        # print(self._sonic_battery_level)
+        # print(self._sonic_name)
+        # print(self._sonic_radio_connection)
+        # print(self._sonic_radio_rssi)
+        # print(self._sonic_serial_no)
+        # print(self._sonic_status)
+        # print(self._sonic_valve_state)
+
+    def _retrieve_signals(self) -> None:
+        """A signal object is a representation of a Signal device (sometimes called hub)
+         that communicates with WiFi and Sonic (a valve installed on a pipe)."""
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self._auth_token}"
+        }
+        response = requests.get(LIST_SIGNALS_RESOURCE, headers=headers)
+        response_data = response.json()
+        print(response_data)
+        self._signal_total_signals = response_data["total_entries"] #need to do something if total signals > 1
+        self._signal_id = response_data["data"][0]["id"]
+        self._signal_boot_time = response_data["data"][0]["boot_time"]
+        self._signal_cloud_connection = response_data["data"][0]["cloud_connection"]
+        self._signal_modem_boot_time = response_data["data"][0]["modem_boot_time"]
+        self._signal_modem_version = response_data["data"][0]["modem_version"]
+        self._signal_name = response_data["data"][0]["name"]
+        self._signal_serial_no = response_data["data"][0]["serial_no"]
+        self._signal_version = response_data["data"][0]["version"]
+        self._signal_wifi_rssi = response_data["data"][0]["wifi_rssi"]
+        # print(self._signal_total_signals)
+        # print(self._signal_id)
+        # print(self._signal_boot_time)
+        # print(self._signal_cloud_connection)
+        # print(self._signal_modem_boot_time)
+        # print(self._signal_modem_version)
+        # print(self._signal_name)
+        # print(self._signal_serial_no)
+        # print(self._signal_version)
+        # print(self._signal_wifi_rssi)
