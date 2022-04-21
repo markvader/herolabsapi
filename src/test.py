e6b746cf2e7a1313761e3813f194bf519ca964a7
Mark Breen <markvader@users.noreply.github.com>
2022-04-21 01:28:01 +0100

    organise functions, provide explanation of usage

--- src/test.py
+++ src/test.py
@@ -8,18 +8,33 @@ herolabs_password = config['DEFAULT']['PASSWORD']
 
 apiObj = Api(herolabs_email, herolabs_password)
 
-login = apiObj._retrieve_token()
-user_details = apiObj._user_details()
-# sonics = apiObj._retrieve_sonics()
+# functions below should all work independently of each other, uncomment one or more of them
+
+# User Details Function
+# user_details = apiObj._user_details()
+
+# Signal Device Function
 # signals = apiObj._retrieve_signals()
+
+# Sonic Device Functions
+# sonics = apiObj._retrieve_sonics()
+telemetry = apiObj._sonic_telemetry()
+# valve_control = apiObj._sonic_valve("close")
+# valve_control = apiObj._sonic_valve("open")
 # incidents = apiObj._retrieve_incidents()
-# telemetry = apiObj._sonic_telemetry()
+
+# property functions
 # properties = apiObj._retrieve_properties()
 # property_notification_settings = apiObj._property_notification_settings()
 # property_settings = apiObj._property_settings()
-# valve_control = apiObj._sonic_valve("close")
-# valve_control = apiObj._sonic_valve("open")
+
+# admin functions
 # update_user_details = apiObj._update_user_details({'last_name': 'testLastName', 'language': 'en'})
-# refresh_token = apiObj._refresh_token()
 # invalidate_token = apiObj._invalidate_token()
-reset_password_request = apiObj._reset_password_request("useremail@emailprovider.com")
+# reset_password_request = apiObj._reset_password_request("useremail@emailprovider.com")
+
+
+# called within other functions and not directly
+# _retrieve_token()
+# _check_token()
+# _refresh_token()
