5f072343e46edec2b7a89b93f901e10dbf22f254
Mark Breen <markvader@users.noreply.github.com>
2022-04-21 01:21:25 +0100

    add token expiry and token handling in all functions

--- src/api.py
+++ src/api.py
@@ -1,5 +1,5 @@
 import requests
-from datetime import datetime
+from datetime import datetime, timedelta
 import json
 from typing import Optional
 from const import (
@@ -30,35 +30,24 @@ class Api:
         """
         self._herolabs_email: str = herolabs_email
         self._herolabs_password: str = herolabs_password
-        self._user_id: Optional[str] = None
+
         self._auth_token: Optional[str] = None
         self._auth_token_expiration: Optional[datetime] = None
+        self._token_renewal_time: Optional[datetime] = None
+
+        self._user_id: Optional[str] = None
         self._property_id: Optional[str] = None
         self._incident_id: Optional[str] = None
 
         self._sonic_total_sonics: Optional[int] = None
+        self._signal_total_signals: Optional[int] = None
+
         self._sonic_id: Optional[str] = None
-        # self._sonic_auto_shutoff_enabled: Optional[bool] = None
-        # self._sonic_auto_shutoff_time_limit: Optional[int] = None
-        # self._sonic_auto_shutoff_volume_limit: Optional[int] = None
-        # self._sonic_battery_level: Optional[str] = None
+        self._signal_id: Optional[str] = None
+
         # self._sonic_name: Optional[str] = None
-        # self._sonic_radio_connection: Optional[str] = None
-        # self._sonic_radio_rssi: Optional[int] = None
-        # self._sonic_serial_no: Optional[str] = None
         # self._sonic_status: Optional[str] = None
         # self._sonic_valve_state: Optional[str] = None
-
-        self._signal_total_signals: Optional[int] = None
-        self._signal_id: Optional[str] = None
-        # self._signal_boot_time: Optional[int] = None
-        # self._signal_cloud_connection: Optional[str] = None
-        # self._signal_modem_boot_time: Optional[int] = None
-        # self._signal_modem_version: Optional[str] = None
-        # self._signal_name: Optional[str] = None
-        # self._signal_serial_no: Optional[str] = None
-        # self._signal_version: Optional[str] = None
-        # self._signal_wifi_rssi: Optional[int] = None
 
     def _retrieve_token(self) -> None:
         """this sends a request to get an auth token
@@ -83,27 +72,78 @@ class Api:
         self._auth_token = response_data["token_details"]
         self._user_id = response_data["user_details"]["id"]
         self._user_email = response_data["user_details"]["email"]
-        print("retrieve token data: " + response.text)
+        self._auth_token_expiration = datetime.now() + timedelta(days=10)  # I am expiring the token after 10 days
+        self._token_renewal_time = self._auth_token_expiration - timedelta(days=2)  # token refreshes after 8 days
+        print("retrieve token data: ", response.text)
+
+    def _check_token(self):
+        """Check token expiry time."""
+        # if no token exists, get one
+        if self._auth_token is None:
+            # print("no token, - acquiring now")
+            self._retrieve_token()
+            # print("token acquired, new expiry time: ", self._auth_token_expiration)
+            return
+        if datetime.now() < self._token_renewal_time:
+            # print("token date valid")
+            # print(self._auth_token_expiration)
+            return
+        if self._token_renewal_time <= datetime.now() < self._auth_token_expiration:
+            # print("token expiring soon, requesting updated token")
+            self._refresh_token()
+            # print("new token expiry time: ", self._auth_token_expiration)
+            return
+        else:
+            # print("no valid or in-date token")
+            self._retrieve_token()
+            # print("token acquired")
+            # print("new token expiry time: ", self._auth_token_expiration)
+            return
 
     def _authenticated_headers(self):
-        """Returns an access token's owner details."""
+        """Returns a dict header for use within subsequent api calls"""
         headers = {
             "Content-Type": "application/json",
             "Authorization": f"Bearer {self._auth_token}"
         }
         return headers
 
+    def _refresh_token(self):
+        """Refresh access token"""
+        response = requests.put(
+            REFRESH_TOKEN_RESOURCE,
+            headers=self._authenticated_headers(),
+        )
+        response_data = response.json()
+        self._auth_token = response_data["token_details"]
+        self._auth_token_expiration = datetime.now() + timedelta(days=10)  # I am expiring the token after 10 days
+        self._token_renewal_time = self._auth_token_expiration - timedelta(days=2)  # token refreshes after 8 days
+        print("Refresh Token: ", response.text)
+
+    def _invalidate_token(self):
+        """Refresh access token"""
+        response = requests.delete(
+            SIGN_OUT_RESOURCE,
+            headers=self._authenticated_headers(),
+        )
+        self._auth_token: Optional[str] = None
+        self._auth_token_expiration: Optional[str] = None
+        self._token_renewal_time: Optional[str] = None
+        print("invalidate_token status_code: ", str(response.status_code), "Token Invalidated. ", response.text)
+
     def _user_details(self):
+        self._check_token()
         """Returns an access token's owner details."""
         user_details_url = USER_RESOURCE + self._user_id
         response = requests.get(
             user_details_url,
             headers=self._authenticated_headers(),
         )
-        print("user_details: " + response.text)
+        print("user_details: ", str(response.text))
 
     def _update_user_details(self, user_updates_payload) -> None:
         """Updates an access token's owner details."""
+        self._check_token()
         update_user_details_url = UPDATE_USER_RESOURCE + self._user_id
         # For multiple changes use comma separation dictionary {'last_name': 'testLastName', 'language': 'en'}
         # These are passed as arguments in the function.
@@ -114,33 +154,17 @@ class Api:
             headers=self._authenticated_headers(),
             data=json.dumps(user_updates_payload)
         )
-        print("status_code: " + str(response.status_code) + " updated_user_details: " + response.text)
-
-    def _refresh_token(self):
-        """Refresh access token"""
-        response = requests.put(
-            REFRESH_TOKEN_RESOURCE,
-            headers=self._authenticated_headers(),
-        )
-        print("Refresh Token: " + response.text)
-
-    def _invalidate_token(self):
-        """Refresh access token"""
-        response = requests.delete(
-            SIGN_OUT_RESOURCE,
-            headers=self._authenticated_headers(),
-        )
-        self._auth_token: Optional[str] = None
-        print("invalidate_token status_code: " + str(response.status_code) + "Token Invalidated. " + response.text)
+        print("status_code: ", str(response.status_code), " updated_user_details: ", response.text)
 
     def _retrieve_sonics(self) -> None:
+        self._check_token()
         """this sends a request to get a list of sonic devices token"""
         response = requests.get(
             LIST_SONICS_RESOURCE,
             headers=self._authenticated_headers()
         )
         response_data = response.json()
-        print("retrieve_sonics: " + response.text)
+        print("retrieve_sonics: ", response.text)
         self._sonic_total_sonics = response_data["total_entries"]
         # For now, I am only storing the first sonic device
         self._sonic_id = response_data["data"][0]["id"]
@@ -154,20 +178,9 @@ class Api:
         self._sonic_serial_no = response_data["data"][0]["serial_no"]
         self._sonic_status = response_data["data"][0]["status"]
         self._sonic_valve_state = response_data["data"][0]["valve_state"]
-        # print(self._sonic_total_sonics)
-        # print(self._sonic_id)
-        # print(self._sonic_auto_shutoff_enabled)
-        # print(self._sonic_auto_shutoff_time_limit)
-        # print(self._sonic_auto_shutoff_volume_limit)
-        # print(self._sonic_battery_level)
-        # print(self._sonic_name)
-        # print(self._sonic_radio_connection)
-        # print(self._sonic_radio_rssi)
-        # print(self._sonic_serial_no)
-        # print(self._sonic_status)
-        # print(self._sonic_valve_state)
 
     def _retrieve_signals(self) -> None:
+        self._check_token()
         """A signal object is a representation of a Signal device (sometimes called hub)
          that communicates with Wi-Fi and Sonic (a valve installed on a pipe)."""
         response = requests.get(
@@ -175,7 +188,7 @@ class Api:
             headers=self._authenticated_headers()
         )
         response_data = response.json()
-        print("retrieve_signals: " + response.text)
+        print("retrieve_signals: ", response.text)
         self._signal_total_signals = response_data["total_entries"]
         # For now, I am only storing the first signal device
         self._signal_id = response_data["data"][0]["id"]
@@ -187,18 +200,9 @@ class Api:
         self._signal_serial_no = response_data["data"][0]["serial_no"]
         self._signal_version = response_data["data"][0]["version"]
         self._signal_wifi_rssi = response_data["data"][0]["wifi_rssi"]
-        # print(self._signal_total_signals)
-        # print(self._signal_id)
-        # print(self._signal_boot_time)
-        # print(self._signal_cloud_connection)
-        # print(self._signal_modem_boot_time)
-        # print(self._signal_modem_version)
-        # print(self._signal_name)
-        # print(self._signal_serial_no)
-        # print(self._signal_version)
-        # print(self._signal_wifi_rssi)
 
     def _retrieve_incidents(self) -> None:
+        self._check_token()
         """An incident is created whenever the hero labs platform
          detects leakage, disconnection, low battery etc."""
         response = requests.get(
@@ -206,7 +210,7 @@ class Api:
             headers=self._authenticated_headers()
         )
         response_data = response.json()
-        print("retrieve_incidents: " + response.text)
+        print("retrieve_incidents: ", response.text)
         self._incidents_total_incidents = response_data["total_entries"]
         # Currently, I am only recording the latest event, I will likely use the feed to build full history
         # Could also filter open/live incidents
@@ -217,18 +221,9 @@ class Api:
         self._incidents_severity = response_data["data"][0]["severity"]
         self._incidents_state = response_data["data"][0]["state"]
         self._incidents_type = response_data["data"][0]["type"]
-        # print(self._incidents_total_signals)
-        # print(self._incidents_id)
-        # print(self._incidents_boot_time)
-        # print(self._incidents_cloud_connection)
-        # print(self._incidents_modem_boot_time)
-        # print(self._incidents_modem_version)
-        # print(self._incidents_name)
-        # print(self._incidents_serial_no)
-        # print(self._incidents_version)
-        # print(self._incidents_wifi_rssi)
 
     def _sonic_telemetry(self):
+        self._retrieve_sonics()
         """A telemetry object contains the latest telemetry details from a Sonic
          such as pressure, temperature etc."""
         sonic_telemetry_url = LIST_TELEMETRY_RESOURCE + self._sonic_id + "/telemetry"
@@ -241,13 +236,10 @@ class Api:
         self._telemetry_pressure = response_data["pressure"]  # pressure is reported as millibar e.g 4914 = 4.914 bar
         self._telemetry_water_flow = response_data["water_flow"]
         self._telemetry_water_temp = response_data["water_temp"]
-        print("sonic_telemetry: " + response.text)
-        # print(self._telemetry_probe_time)
-        # print(self._telemetry_pressure)
-        # print(self._telemetry_water_temp)
-        # print(self._telemetry_water_flow)
+        print("sonic_telemetry: ", response.text)
 
     def _retrieve_properties(self) -> None:
+        self._check_token()
         """Property is the main object, and it may represent a single flat, house, apartment etc.
         It has an owner and all other objects like a sonic, signal, incidents and others
         are either directly or indirectly linked to a property."""
@@ -256,7 +248,7 @@ class Api:
             headers=self._authenticated_headers(),
         )
         response_data = response.json()
-        print("retrieve_properties: " + response.text)
+        print("retrieve_properties: ", response.text)
         self._properties_total_properties = response_data["total_entries"]
         # For now, I am only storing the first property
         self._property_id = response_data["data"][0]["id"]
@@ -269,10 +261,9 @@ class Api:
         self._property_long = response_data["data"][0]["lng"]
         self._property_postcode = response_data["data"][0]["postcode"]
         self._property_uprn = response_data["data"][0]["uprn"]
-        # print(self._properties_total_properties)
-        # print(self._property_id)
 
     def _property_notification_settings(self) -> None:
+        self._retrieve_properties()
         """Part of the property object is notification settings where a user can configure
          what notifications they would like to receive."""
         property_notification_settings_url = FETCH_NOTIFICATION_SETTINGS_RESOURCE + self._property_id + "/notifications"
@@ -290,9 +281,10 @@ class Api:
         self._property_notifications_pressure_test_failed = response_data["pressure_test_failed"]
         self._property_notifications_pressure_test_skipped = response_data["pressure_test_skipped"]
         self._property_notifications_radio_disconnection = response_data["radio_disconnection"]
-        print("property_notifications: " + response.text)
+        print("property_notifications: ", response.text)
 
     def _property_settings(self) -> None:
+        self._retrieve_properties()
         """Part of the property object is settings where a user can configure timezone, webhook etc."""
         property_settings_url = FETCH_PROPERTY_SETTINGS_RESOURCE + self._property_id + "/settings"
         response = requests.get(
@@ -306,9 +298,10 @@ class Api:
         self._property_settings_timezone = response_data["timezone"]
         self._property_settings_webhook_enabled = response_data["webhook_enabled"]
         self._property_settings_webhook_url = response_data["webhook_url"]
-        print("property_settings: " + response.text)
+        print("property_settings: ", response.text)
 
     def _sonic_valve(self, valve_action: str):
+        self._retrieve_sonics()
         """A telemetry object contains the latest telemetry details from a Sonic
          such as pressure, temperature etc."""
         sonic_valve_url = SONIC_VALVE_RESOURCE + self._sonic_id + "/valve"
@@ -319,10 +312,11 @@ class Api:
                 "action": f"{valve_action}",  # options are open or close
             }
         )
-        print("sonic_valve status_code: " + str(response.status_code) + ", valve will now " + valve_action
+        print("sonic_valve status_code: ", str(response.status_code), ", valve will now ", valve_action
               + response.text)
 
     def _reset_password_request(self, user_email: str):
+        self._check_token()
         """Requests reset password email with further instructions. The endpoint returns always
         http status 204 regardless of email existence."""
         response = requests.post(
@@ -332,4 +326,4 @@ class Api:
                 "email": f"{user_email}",  # options are open or close
             }
         )
-        print("reset_password_request status_code: " + str(response.status_code) + response.text)
+        print("reset_password_request status_code: ", str(response.status_code), response.text)
