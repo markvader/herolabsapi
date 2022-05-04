e7e250b75011539659719e429c888d621cf9a81b
markvader <markjbreen@gmail.com>
2022-05-04 01:26:12 +0100

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='herolabsapi',
+    version='0.1.0',
+    packages=['src'],
+    url='https://github.com/markvader/herolabsapi',
+    license='MIT',
+    author='Mark Breen',
+    author_email='markjbreen@gmail.com',
+    description='Python Library for Hero Labs API, creators of the Sonic smart water shut off valve'
+)
