25a0a80b8ca292438a200a21192b813d2c0d2234
markvader <markjbreen@gmail.com>
2022-05-04 01:54:57 +0100

    updates to setup.py file

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='herolabsapi',
-    version='0.1.0',
+    version='0.1.1',
     packages=['src'],
     url='https://github.com/markvader/herolabsapi',
     license='MIT',
     author='Mark Breen',
     author_email='markjbreen@gmail.com',
-    description='Python Library for Hero Labs API, creators of the Sonic smart water shut off valve'
+    description='Python Library for Hero Labs API, creators of the Sonic smart water shut off valve',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
